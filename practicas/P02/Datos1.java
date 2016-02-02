aec93ccbcac898e5053cbf70726bcd80f1c12aaf
lili-flo@hotmail.com <lili-flo@hotmail.com>
2016-02-02 15:28:59 -0600

    Estructura del repositorio

---
+++ practicas/P02/Datos1.java
@@ -0,0 +1,32 @@
+public class Datos1 {
+
+byte docena1;
+short edad;
+int capacidad;
+long masa;
+char letra1;
+float diametro;
+double peso;
+
+public static void main(String[] args) {
+
+byte docena1 = 12;
+short edad = 19;
+int capacidad = 1500;
+long masa = 178823411L;
+char letra1 = 'h';
+float diametro = 34.25f;
+double peso = 88.77;
+
+// Para datos mas precicsos usamos clases que son los mismos pero con mayusculas. Short. Int. Long.. .. etc.
+System.out.println("Tipos de datos son:");
+System.out.println(docena1);
+System.out.println(edad);
+System.out.println(capacidad);
+System.out.println(masa);
+System.out.println(letra1);
+System.out.println(diametro);
+System.out.println(peso);
+}
+
+}
