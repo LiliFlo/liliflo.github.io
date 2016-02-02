ca23a4dba75efc5b0d02d92ff22d6a9a812806f3
lili-flo@hotmail.com <lili-flo@hotmail.com>
2016-02-02 14:21:38 -0600

    Estructura del repositorio

---
+++ practicas/P02/Datos.java
@@ -0,0 +1,41 @@
+public class Datos {
+
+public static void main(String[] args) {
+
+// Tipos de datos
+//Tipos primitivos. Numericos. Enteros
+//docena1 = 12 // Entero tipo byte 
+//edad = 19; // Entero tipo short
+//capacidad = 1500; // Entero tipo int
+//masa = 178823411L; // Entero tipo long. Una l o L final indica que es long.
+//letra1 = ‘h’; // Tipo char .caracter. Se escribe entre comillas simples.
+
+//Tipos primitivos. Numericos. Reales
+//diametro = 34.25f; // Tipo float. Una f o F final indica que es float.
+//peso = 88.77; // Tipo double. Un numero con punto decimal se interpreta normalmente como double.
+// Tipos Referenciados. 
+//arreglo1; // Tipo Arreglos Array.
+//cadena1="hola mundo"; // Cadena de caracteres tipo String.
+// Boolean  true / false
+//funciona = true; // Tipo boolean
+//esVisible = false; // Tipo boolean
+byte docena1 = 12;
+short edad = 19;
+int capacidad = 1500;
+long masa = 178823411L;
+char letra1 = 'h';
+float diametro = 34.25f;
+double peso = 88.77;
+
+// Para datos mas precicsos usamos clases que son los mismos pero con mayusculas. Short. Int. Long.. .. etc.
+System.out.println("Tipos de datos son:");
+System.out.println(docena1);
+System.out.println(edad);
+System.out.println(capacidad);
+System.out.println(masa);
+System.out.println(letra1);
+System.out.println(diametro);
+System.out.println(peso);
+}
+
+}
