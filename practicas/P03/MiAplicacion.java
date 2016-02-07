49b3e3db83af783da36220620a48b8c72b27e5dd
lili-flo@hotmail.com <lili-flo@hotmail.com>
2016-02-06 21:37:44 -0600

    Estructura del repositorio

---
+++ practicas/P03/MiAplicacion.java
@@ -0,0 +1,16 @@
+public class MiAplicacion {
+
+public static void main(String[] args) {
+// ax3+bx2+cx+d
+
+double c3 = 1, c2 = 2, c1 = 3, c0 = 4;
+double x = 2;
+double resultado;
+
+resultado = c3*x*x*x + c2*x*x + c3*x + c0;
+
+//
+System.out.println(resultado);
+} // Fin del main
+
+} // Fin de la clase MiAplicacion
