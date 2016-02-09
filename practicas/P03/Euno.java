5de3b1b69ce1d30bdb8fb33805220a4ee0f1c506
lili-flo@hotmail.com <lili-flo@hotmail.com>
2016-02-09 11:23:52 -0600

    Estructura del repositorio

---
+++ practicas/P03/Euno.java
@@ -0,0 +1,23 @@
+public class Euno { 
+
+public static void main(String[] args) {
+// ax3+bx2+cx+d
+
+int a = 10, b = 3, c = 1, d, e;
+float x, y;
+boolean f = a > b; // valor booleano se declara en true
+x = a/b;
+f = a < b;  // si no coincide con el valor "true" el resultado es "false
+// original c = a < b && c; quitamos el valor c y operador &&  
+d = a +  b++;
+e = ++a - b;
+y = (float)a/b;
+//
+System.out.println(x);
+System.out.println(f);
+System.out.println(d);
+System.out.println(e);
+System.out.println(y);
+
+} // Fin del main
+} // Fin de la clase EjerciciounoPtres
--- practicas/P03/MiAplicacion.java
+++ practicas/P03/MiAplicacion.java
@@ -4,7 +4,7 @@ public static void main(String[] args) {
 // ax3+bx2+cx+d
 
 double c3 = 1, c2 = 2, c1 = 3, c0 = 4;
-double x = 2;
+double x = 3;
 double resultado;
 
 resultado = c3*x*x*x + c2*x*x + c3*x + c0;
