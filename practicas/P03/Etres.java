a208da1c97c497003ad1b56196db270f739aa2cd
lili-flo@hotmail.com <lili-flo@hotmail.com>
2016-02-09 13:26:30 -0600

    Estructura del repositorio

--- practicas/P03/Etres.java
+++ practicas/P03/Etres.java
@@ -1,19 +1,15 @@
+import java.util.Scanner;
+
 public class Etres {
 
 public static void main(String[] args) {
 
-int numero;
-//Multiplos de 5 hasta 100
-//Ciclo for desde 1 a 100
-//Validamos que numeros son multiplos de 5 con condicional if
-//Va a dividir 5 entre 100 y si el residuo es 0 se cumple la condicion 
-String mul="";
-for (int i = 1; i < 100; i++){
-	if (100%5==0){
-		mul=mul+1;
-	}
-}
-System.out.print("Multiplos de 5 de 1 a 100" + mul);
-} // Fin del main
+Scanner entrada = new Scanner(System.in);
 
-} // Fin de la clase EjerciciotresPtres
+int numero;
+
+System.out.print("Ingresar un numero:");
+numero = entrada.nextInt();
+System.out.print("La suma de los primeros " + numero + " numeros multiplos de 5 es de:" + ((numero*numero * 5) + (numero * 5)) / 2);
+}
+}
